40e3425ca829f20fa498e60755419e3d16ca52ba
zhu2qian1 <zhu2qian1@gmail.com>
2021-01-31 15:39:00 +0900

    Update pg.py
    simplified

--- src/pg.py
+++ src/pg.py
@@ -1,63 +1,62 @@
-DEBUG = False
-
-
 class Unit:
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.name: str = name
-        self.parentof: list = []
-        self.childof: list = []
+        self.__id: str = ""
+        self.__child: list = []
+        self.__parent: list = []
 
-    def __str__(self):
-        return self.name
 
-    def __repr__(self):
-        return self.__str__()
+class UnitBase(Unit):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
 
-    def adopt(self, child):
-        # nobody can either adopt somebody who have already existed or adopt oneself.
+    def adopt(self, child: Unit) -> None:
         if child is self:
             raise ValueError(f"{self} cannot adopt itself.")
-        elif child in self.parentof:
-            raise ValueError(
-                f"{self} cannot adopt {child} for {self} is already parent of {child}"
-            )
+        elif child in self.__child:
+            raise ValueError(f"{child} is already a child of {self}.")
         else:
-            self.parentof.append(child)
-            child.childof.append(self)
-        if DEBUG:
-            print(f"{self} adopted {child}. Children of {self} are {self.parentof}.")
+            self.__child.append(child)
+            child.__parent.append(self)
 
-    def abandon(self, child):
-        # nobody can abandon somebody who does not exist or abandon oneself.
+    def abandon(self, child: Unit) -> None:
         if child is self:
-            raise ValueError(f"{self} cannot abandon itself")
-        elif child not in self.parentof:
-            raise ValueError(
-                f"{self} cannot abandon {child} for {self} is not parent of {child}"
-            )
+            raise ValueError(f"{self} cannot abandon itself.")
+        elif child not in self.__child:
+            raise ValueError(f"{child} is not a child of {self}.")
         else:
-            self.parentof.remove(child)
-            child.childof.remove(self)
-        if DEBUG:
-            print(f"{self} abandoned {child}. Children of {self} are {self.parentof}.")
+            self.__child.remove(child)
+            child.__parent.remove(self)
 
-    def _tellfamily(self):
-        print(f"{self} is parent of {self.parentof} and child of {self.childof}.")
+    def join(self, parent: Unit) -> None:
+        if parent is self:
+            raise ValueError
+        elif parent in self.__parent:
+            raise ValueError
+        else:
+            pass
+
+    def leave(self, parent: Unit) -> None:
+        if parent is self:
+            raise ValueError
+        elif parent not in self.__parent:
+            raise ValueError
+
+        else:
+            pass
 
 
-units = []
-units.append((aunit := Unit("Unit A")))
-units.append((bunit := Unit("Unit B")))
-units.append((cunit := Unit("Unit C")))
-units.append((dunit := Unit("Unit D")))
+class Task:
+    pass
 
-aunit.adopt(bunit)
-aunit.adopt(dunit)
-aunit.adopt(cunit)
-cunit.adopt(aunit)
-bunit.adopt(cunit)
-aunit.abandon(bunit)
-cunit.abandon(aunit)
-print("------------")
-for unit in units:
-    unit._tellfamily()
+
+class Project:
+    pass
+
+
+class Person:
+    pass
+
+
+class Team:
+    pass
